cb398e626207c0c4d7e32d52c998f08e335f5e22
Tom Flídr <tomflidr@gmail.com>
2021-03-02 18:47:34 +0100

    new version

--- src/MvcCore/Ext/Controllers/DataGrid.php
+++ src/MvcCore/Ext/Controllers/DataGrid.php
@@ -20,6 +20,6 @@ implements	\MvcCore\Ext\Controllers\IDataGrid {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.0';
+	const VERSION = '5.0.1';
 
 }
