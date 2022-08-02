0140951f0fd41ff4ab3d00e45989a902b4f2454c
Tom Flídr <tomflidr@gmail.com>
2022-08-02 11:32:16 +0200

    release v5.1.13

--- src/MvcCore/Ext/Controllers/DataGrid.php
+++ src/MvcCore/Ext/Controllers/DataGrid.php
@@ -31,5 +31,5 @@ implements	\MvcCore\Ext\Controllers\IDataGrid {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.1.12';
+	const VERSION = '5.1.13';
 }
